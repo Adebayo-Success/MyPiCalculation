0c2b2071e8e1606ff9e70db0ac56c41a1119971b
Adebayo khalid <khalidadebayo18@gmail.com>
2024-12-09 23:01:50 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Declare some variables
+
+PI();
+void PI()
+{
+    try
+    {   
+        Console.Write("Input the first Real Number : ");
+        float floatPI = float.Parse(Console.ReadLine()!);
+        Console.Write("Input the second Real Number : ");
+        double doublePI = double.Parse(Console.ReadLine()!);
+        // Print the results on the console
+        Console.WriteLine("Float PI is: " + floatPI, + doublePI);
+        // Console.WriteLine("Double PI is: " + doublePI);
+    }
+    catch (Exception)
+    {     
+        Console.Write(" Invalid Real Number : ");
+    }
+}
+
+
